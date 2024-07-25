a0e9b31d82c20ce5beb0a2497c8c8073f97c0c7a
hadikp <hp20001025@gmail.com>
2024-07-25 21:51:15 +0200

    add pages

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,9 @@ const routes = [
       {path: '/', name: 'home', component: () => import('../views/HomeView.vue')},
       {path: '/about', name: 'about', component: () => import('../views/AboutView.vue')},
       {path: '/artWork', name: 'artWork', component: () => import('../views/ArtWorksView.vue')},
-      {path: '/firebase', name: 'firebase', component: () => import('../views/Firebase.vue')}
+      {path: '/firebase', name: 'firebase', component: () => import('../views/Firebase.vue')},
+      {path: '/exhibition', name: 'exhibition', component: () => import('../views/ExhibitionView.vue')},
+      {path: '/contact', name: 'contact', component: () => import('../views/ContactView.vue')}
   ]
   
   const router = createRouter({
---
+++ src/views/ContactView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="exh">
+    <h1 class="p-3 mb-2 bg-warning text-dark">This is a Contact page</h1>
+  </div>
+
+</template>
+
+<style>
+
+</style>
