572cbce8d6ff5f212b784bf6f0719e21bd9c57a6
hadikp <hp20001025@gmail.com>
2024-08-12 19:24:09 +0200

    blog first post

--- blog/.vitepress/config.js
+++ blog/.vitepress/config.js
@@ -3,24 +3,15 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Painting Blog",
-  description: "A VitePress Site",
+  description: "I write about my painting",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
     ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-
+    search: {
+      provider: 'local'
+    },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ]
---
+++ blog/article-1.md
@@ -0,0 +1,13 @@
+---
+title: My first post
+---
+
+# My first post
+
+Lorem ipsum dolor sit amet. Non voluptas deleniti non laudantium magnam ea culpa ipsam At reiciendis officia aut nulla placeat qui magnam natus.  
+
+## Sub Section
+Est commodi iusto et alias deleniti sed voluptatibus tempora est reprehenderit autem ab debitis quisquam non tenetur aliquid. Aut quod pariatur ut inventore aliquam sed rerum nihil aut voluptas consequatur et adipisci doloribus.  
+
+## Another Sub Section
+Ea consequuntur veniam ut veniam laborum sit recusandae deserunt. Quo ipsa odit ut temporibus ipsam cum consequatur itaque et possimus tempore et quia cumque!
