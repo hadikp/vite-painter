ea517436c2012e2291257fc9caa4f39b62ca3f16
hadikp <hp20001025@gmail.com>
2024-12-08 15:08:52 +0100

    add new vitepress blog

--- blog/.vitepress/config.js
+++ blog/.vitepress/config.js
@@ -2,16 +2,25 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Painting Blog",
-  description: "I write about my painting",
+  title: "My Blog",
+  description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
     ],
-    search: {
-      provider: 'local'
-    },
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ]
